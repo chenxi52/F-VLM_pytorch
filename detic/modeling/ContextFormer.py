71394d161f53531d8b4f207a6df4c138e5b794c9
chenxi105 <1051180307@qq.com>
2023-12-14 16:49:35 +0800

    add pe to visual_emb

--- detic/modeling/ContextFormer.py
+++ detic/modeling/ContextFormer.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 from torch import nn, Tensor
 import numpy as np
 from timm.models.layers import trunc_normal_
-
+from detic.modeling.sam.modeling.prompt_encoder import PositionEmbeddingRandom
 def _get_activation_fn(activation):
     """Return an activation function given a string"""
     if activation == "relu":
@@ -180,6 +180,7 @@ class build_contextformer(nn.Module):
         if use_ln:
             self.ln_mask = nn.LayerNorm(d_model)
         self.dropout = nn.Dropout()
+        self.pe = PositionEmbeddingRandom(d_model)
 
     def get_qs(self, q, cls):
         # concat[cls*txt,txt]
@@ -200,9 +201,9 @@ class build_contextformer(nn.Module):
             q = self.ln_mask(self.q_proj(mask_token))
         else:
             q = self.q_proj(mask_token)
-        # mask_text = self.decoder1(q, kv)
-
-        mask_img = self.decoder2(q, visual_tokens)
+        mask_text = self.decoder1(q, kv)
+        pe = torch.repeat_interleave(self.pe, visual_tokens.shape[0], dim=0)
+        mask_img = self.decoder2(mask_text, visual_tokens, pos=pe)
         return self.get_logits(mask_img.squeeze(), clip_txt)
 
     def get_logits(self, image, text):
