f73c969719a0f7c0fdaf28dc6176c79b5ccef785
chenxi105 <1051180307@qq.com>
2024-01-05 10:25:45 +0800

    reform configs

--- run_fvlm.sh
+++ run_fvlm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=FVLM
+#SBATCH --job-name=FVLM_1x
 #SBATCH --partition=vip1
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=128
@@ -9,6 +9,5 @@
 
 
 srun python train_net_stand.py --config-file configs/Fvlm_tiny_coco.yaml --num-gpus 8 \
-    OUTPUT_DIR output/clipRpn/FVLM/biCrossEntropy\
-    MODEL.ROI_BOX_HEAD.USE_SIGMOID_CE True\
-    SOLVER.BASE_LR 0.0002
+    OUTPUT_DIR output/clipRpn/FVLM/stand_1x\
+    MODEL.ROI_BOX_HEAD.USE_SIGMOID_CE False\
