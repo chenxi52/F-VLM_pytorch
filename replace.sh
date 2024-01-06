bd8c90de536b7d9aa3a47aeeb26db7fef63e7571
0_29 <1051180307@qq.com>
2024-01-06 15:08:42 +0800

    reform config file

--- replace.sh
+++
@@ -1,8 +0,0 @@
-echo 'install detectron2'
-# python3 -m pip install 'git+https://github.com/facebookresearch/detectron2.git'
-sudo rm -rf ../detectron2/detectron2/utils/events.py
-sudo cp -r ./events.py ../detectron2/detectron2/utils/
-
-
-sudo rm -rf ../detectron2/detectron2/utils/events.py
-sudo cp -r ./events.py ../detectron2/detectron2/utils/
--- vis.sh
+++
@@ -1,3 +0,0 @@
-python3 tools/visualize_json_results.py \
-        --input /opt/tiger/OpenVo-Seg-master/output/test/inference_coco_2017_val/coco_instances_results.json \
-        --output output/visualize/
